aba02c105acb279dd5c107b5afd7b182781b7bb6
AdityaSai2004 <2004sai@gmail.com>
2022-04-26 08:46:37 +0530

    Day1

---
+++ Day1/sum_of_2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("addin.txt","r",stdin);
+    freopen("addout.txt","r",stdout);
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    return 0;
+}
