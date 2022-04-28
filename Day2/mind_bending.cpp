d0224874e943b0201cdeb3ed00cbac2dba7d941e
AdityaSai2004 <2004sai@gmail.com>
2022-04-28 14:14:04 +0530

    changed

--- Day2/mind_bending.cpp
+++ Day2/mind_bending.cpp
@@ -7,17 +7,24 @@ int main(){
     int x3,y3,x4,y4;
     cin>>x1>>y1>>x2>>y2;
     cin>>x3>>y3>>x4>>y4;
-    int A1=(x2-x1)*(y2-y1);
-    int A2=(x4-x3)*(y4-y3);
-    int left=max(x1,x3);
-    int right=min(x2,x4);
-    int bottom=max(y1,y3);
-    int top=min(y2,y4);
-    int A_intersection=0;
-    if((left<right) && (bottom<top)){
-        int A_intersection=(right-left)*(top-bottom);
+    int a_width=x2-x1;
+    int a_height=y2-y1;
+    int b_width=x4-x3;
+    int b_height=y4-y3;
+    int a_area=a_width*a_height;
+    int b_area=b_width*b_height;
+    int total_area=a_area+b_area;
+    if(x3<x2 || x1<x4 || y3<y2 || y1<y4){
+        int left=max(x1,x3);
+        int right=min(x2,x4);
+        int bottom=max(y1,y3);
+        int top=min(y2,y4);
+        int a_intersection=0;
+        if((left<right) && (bottom<top)){
+            a_intersection=(right-left)*(top-bottom);
+        }
+        total_area-=a_intersection;
     }
-    int total_area=(A1+A2)-A_intersection;
-    cout<<total_area;
+    cout<<total_area<<endl;
     return 0;
 }
